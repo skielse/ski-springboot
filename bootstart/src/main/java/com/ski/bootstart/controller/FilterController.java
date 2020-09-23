fdb778924bc99e4871e8032414aa0591eb880394
王梓杰 <wangzijie@koolearn-inc.com>
2020-09-23 18:33:48 +0800

    add FilterController test

--- bootstart/src/main/java/com/ski/bootstart/controller/FilterController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/FilterController.java
@@ -1,4 +1,14 @@
 package com.ski.bootstart.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("filter")
 public class FilterController {
+
+    @RequestMapping("test")
+    public String test() {
+        return "";
+    }
 }
