e49aa0fc2a08acb6deadfb553b926db840778f1e
skielse <marine0228@gmail.com>
2020-11-27 18:27:13 +0800

    add author

--- bootstart/src/main/java/com/ski/bootstart/controller/FilterController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/FilterController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author wangzijie
+ */
 @Slf4j
 @RestController
 @RequestMapping("filter")
