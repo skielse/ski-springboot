7896aafcbe20461f25b77d062974f263f1285082
skielse <marine0228@gmail.com>
2021-05-27 10:24:57 +0800

    add time trans

--- bootstart/src/main/java/com/ski/bootstart/controller/ReflectionController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/ReflectionController.java
@@ -1,6 +1,7 @@
 package com.ski.bootstart.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,16 @@ public class ReflectionController {
         Class<ReflectionController> aClass = ReflectionController.class;
         log.info("class is {}", aClass);
         return aClass;
+    }
+
+    private String getStringTimeBySecond(Integer playerTime) {
+        //时
+        int h = (playerTime / 1000) / 3600;
+        //分钟
+        int m = ((playerTime / 1000) % 3600) / 60;
+        //秒
+        int s = (playerTime / 1000) % 60;
+
+        return StringUtils.join(h < 10 ? "0" + h : h, ":", m < 10 ? "0" + m : m, ":", s < 10 ? "0" + s : s);
     }
 }
