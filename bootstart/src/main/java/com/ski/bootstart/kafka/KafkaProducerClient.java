dd708a30a67f44a9eaa936a11e51ed1cbef14e82
skielse <marine0228@gmail.com>
2022-06-21 15:25:50 +0800

    add KafkaProducerClient

---
+++ bootstart/src/main/java/com/ski/bootstart/kafka/KafkaProducerClient.java
@@ -0,0 +1,53 @@
+package com.ski.bootstart.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * @author wangzijie
+ * @date 2022/6/21
+ */
+public class KafkaProducerClient implements Runnable {
+
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+
+    public KafkaProducerClient(String topic) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "x.x.x.x:9092");
+        //acks=0：如果设置为0，生产者不会等待kafka的响应。
+        //acks=1：这个配置意味着kafka会把这条消息写到本地日志文件中，但是不会等待集群中其他机器的成功响应。
+        //acks=all：这个配置意味着leader会等待所有的follower同步完成。这个确保消息不会丢失，除非kafka集群中所有机器挂掉。这是最强的可用性保证。
+        props.put("acks", "all");
+        //配置为大于0的值的话，客户端会在消息发送失败时重新发送。
+        props.put("retries", 0);
+        //当多条消息需要发送到同一个分区时，生产者会尝试合并网络请求。
+        props.put("batch.size", 16384);
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+        this.producer = new KafkaProducer<String, String>(props);
+        this.topic = topic;
+    }
+
+    public void run() {
+        int messageNo = 1;
+        try {
+            int i = 0;
+            while (i <1000) {
+                String messageStr = "你好，这是第" + messageNo + "条数据";
+                producer.send(new ProducerRecord<String, String>(topic, "Message", messageStr));
+                messageNo++;
+                Thread.sleep(1000);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
+    }
+
+}
