63e5bbaad6269e5797f6d59e0d045a0146f8d36c
skielse <marine0228@gmail.com>
2022-08-18 15:18:54 +0800

    add WebConfig

---
+++ bootstart/src/main/java/com/ski/bootstart/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ski.bootstart.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author wangzijie
+ * @date 2022/8/18
+ */
+@Configuration
+public class WebConfig extends WebMvcAutoConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/home/index");
+    }
+}
