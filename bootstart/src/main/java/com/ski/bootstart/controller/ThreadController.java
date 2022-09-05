af659f1fec8e7fe81a69d51eb02483bb8c25a296
skielse <marine0228@gmail.com>
2022-09-05 15:49:30 +0800

    add callable

--- bootstart/src/main/java/com/ski/bootstart/controller/ThreadController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/ThreadController.java
@@ -1,9 +1,13 @@
 package com.ski.bootstart.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,5 +27,33 @@ public class ThreadController {
             log.error("", e);
         }
         return "test";
+    }
+
+
+    /**
+     * 使用Callable
+     */
+    @GetMapping("callable")
+    public Callable<String> callable() {
+        log.info(LocalDateTime.now().toString() + "--->主线程开始");
+        Callable<String> callable = () -> {
+            String result = "return callable";
+            // 执行业务耗时 5s
+            Thread.sleep(5000);
+            log.info(LocalDateTime.now().toString() + "--->子任务线程(" + Thread.currentThread().getName() + ")");
+            return result;
+        };
+        log.info(LocalDateTime.now().toString() + "--->主线程结束");
+        return callable;
+    }
+
+    public static String doBusiness() {
+        // 执行业务耗时 10s
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return UUID.randomUUID().toString();
     }
 }
