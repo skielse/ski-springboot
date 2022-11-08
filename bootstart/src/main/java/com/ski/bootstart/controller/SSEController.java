59c533930fb4b3aec0da5529c784317c67bab851
skielse <marine0228@gmail.com>
2022-11-08 17:12:17 +0800

    update sse

--- bootstart/src/main/java/com/ski/bootstart/controller/SSEController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/SSEController.java
@@ -20,7 +20,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 @RequestMapping("sse")
 public class SSEController {
-    private static Map<String, SseEmitter> sseCache = new ConcurrentHashMap<>();
+    private static final Map<String, SseEmitter> SSE_CACHE = new ConcurrentHashMap<>();
 
     @GetMapping(path = "")
     public String index() {
@@ -31,14 +31,14 @@ public class SSEController {
     @GetMapping(path = "subscribe", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
     public SseEmitter push(String id) throws IOException {
         // 超时时间设置为30s，用于演示客户端自动重连
-        SseEmitter sseEmitter = new SseEmitter(30000L);
+        SseEmitter sseEmitter = new SseEmitter(20000L);
         // 设置前端的重试时间为10s
         sseEmitter.send(SseEmitter.event().reconnectTime(10000).data("连接成功"));
-        sseCache.put(id, sseEmitter);
+        SSE_CACHE.put(id, sseEmitter);
         log.info("add " + id);
         sseEmitter.onTimeout(() -> {
             log.info(id + "超时");
-            sseCache.remove(id);
+            SSE_CACHE.remove(id);
         });
         sseEmitter.onCompletion(() -> log.info("完成！！！"));
         return sseEmitter;
@@ -46,7 +46,7 @@ public class SSEController {
 
     @GetMapping(path = "push")
     public String push(String id, String content) throws IOException {
-        SseEmitter sseEmitter = sseCache.get(id);
+        SseEmitter sseEmitter = SSE_CACHE.get(id);
         if (sseEmitter != null) {
             sseEmitter.send(content);
         }
@@ -55,10 +55,10 @@ public class SSEController {
 
     @GetMapping(path = "over")
     public String over(String id) {
-        SseEmitter sseEmitter = sseCache.get(id);
+        SseEmitter sseEmitter = SSE_CACHE.get(id);
         if (sseEmitter != null) {
             sseEmitter.complete();
-            sseCache.remove(id);
+            SSE_CACHE.remove(id);
         }
         return "over";
     }
