30433a333a4924691305745e1b3c028165a56b6b
skielse <marine0228@gmail.com>
2023-01-30 15:41:46 +0800

    add itextPdf

---
+++ bootstart/src/main/java/com/ski/bootstart/pdf/PdfHelloWorld.java
@@ -0,0 +1,41 @@
+package com.ski.bootstart.pdf;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author wangzijie
+ * @date 2023/1/30
+ */
+public class PdfHelloWorld {
+
+    public static final String DEST = "/Users/wangzijie/logs/itextpdf/results/chapter01/hello_world.pdf";
+
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new PdfHelloWorld().createPdf(DEST);
+    }
+
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        PdfWriter writer = new PdfWriter(dest);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+
+        // Initialize document
+        Document document = new Document(pdf);
+
+        //Add paragraph to the document
+        document.add(new Paragraph("Hello World!"));
+
+        //Close document
+        document.close();
+    }
+}
