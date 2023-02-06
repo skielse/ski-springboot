a7bb03510c4d4e3156e32dcf4cf5f06fdc51a838
skielse <marine0228@gmail.com>
2023-02-06 11:54:04 +0800

    add itextPdf list

---
+++ bootstart/src/main/java/com/ski/bootstart/pdf/PdfRickAstley.java
@@ -0,0 +1,61 @@
+package com.ski.bootstart.pdf;
+
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.List;
+import com.itextpdf.layout.element.ListItem;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author wangzijie
+ * @date 2023/2/6
+ */
+public class PdfRickAstley {
+    public static final String DEST = "/Users/wangzijie/logs/itextpdf/results/chapter01/rick_astley.pdf";
+
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new PdfRickAstley().createPdf(DEST);
+    }
+
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        PdfWriter writer = new PdfWriter(dest);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+
+        // Initialize document
+        Document document = new Document(pdf);
+
+        // Create a PdfFont
+        PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLDOBLIQUE);
+        // Add a Paragraph
+        document.add(new Paragraph("iText is:").setFont(font));
+        // Create a List
+        List list = new List()
+                .setSymbolIndent(12)
+                .setListSymbol("\u2022")
+                .setFont(font);
+        // Add ListItem objects
+        list.add(new ListItem("Never gonna give you up"))
+                .add(new ListItem("Never gonna let you down"))
+                .add(new ListItem("Never gonna run around and desert you"))
+                .add(new ListItem("Never gonna make you cry"))
+                .add(new ListItem("Never gonna say goodbye"))
+                .add(new ListItem("Never gonna tell a lie and hurt you"));
+        // Add the list
+        document.add(list);
+
+        //Close document
+        document.close();
+    }
+}
