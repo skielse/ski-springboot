d605a02ad3c04a8eac6b92d41781894f4115665e
skielse <marine0228@gmail.com>
2023-08-16 17:02:23 +0800

    add page

--- bootstart/src/main/java/com/ski/bootstart/controller/HomeController.java
+++ bootstart/src/main/java/com/ski/bootstart/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ski.bootstart.controller;
 
+import com.google.gson.Gson;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,23 @@ public class HomeController {
 
     @RequestMapping("index")
     public String index() {
-      log.info("============= Welcome back home =============");
+        log.info("============= Welcome back home =============");
         log.error("this is user error");
-      return "============= Welcome back home =============";
+        return "============= Welcome back home =============";
+    }
+
+    @RequestMapping("page")
+    public String page() {
+        Gson gson = new Gson();
+        HomePage page = new HomePage();
+        page.setId(1);
+        page.setName("Jordan");
+        return gson.toJson(page);
+    }
+
+    @Data
+    static class HomePage {
+        private int id;
+        private String name;
     }
 }
